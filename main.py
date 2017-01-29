7c6385f5b1ee4da49964f7812951af31e571f95f
John Olivo <jolivo2@bsbason.edu>
2017-01-29 17:03:55 -0500

    message was placed inside text area, button created, and encrypt file modified

--- main.py
+++ main.py
@@ -21,7 +21,12 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         message = 'Hellooo world!'
         encrypted_message = caesar.encrypt(message, 13)
-        self.response.write(encrypted_message)
+
+        text_area = "<textarea>" + encrypted_message + "</textarea>"
+        submit = "<input type= 'submit'/>"
+        form = "<form>" + text_area + "<br>" + submit + "</form>"
+
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
